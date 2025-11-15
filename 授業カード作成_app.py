d2d28f19aba8cba1f58c386bce432d43ec35468d
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-11-15 13:44:20 +0900

    Q

--- 授業カード作成_app.py
+++ 授業カード作成_app.py
@@ -123,7 +123,7 @@ def generate_excel_from_form_data(form_data):
         # 例: Googleフォームの項目名 -> Excelのセル
         cell_mappings = {
 
-            '知的段階及び発達段階': 'B2'
+            '知的段階及び発達段階': 'B2',
             '単元名': 'B3',
             'キャッチコピー': 'B5',
             '授業のねらい': 'B9',
